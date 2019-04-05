90a9ed660ffe6dd99bae0b22eaa6e4c92a46c71f
saradelr <saradelr@gmail.com>
2019-04-05 10:16:08 +0200

    update

--- Project/Frontpage/js/products.js
+++ Project/Frontpage/js/products.js
@@ -29,3 +29,56 @@ const findProductById = (data, id) => {
 
     return product
 }
+
+/**
+ * SHOPPING CART
+ */
+
+// Create a function that adds a product by it's ID and its quantity to the shopping cart
+const addToCart = (id, quant) => {
+    console.log(`${id} has ben added ${quant} times`);
+    if (!findProductById(cart, id)) {
+            cart.push({...findProductById(products, id), quantity: quant})
+    } else {
+            findProductById(cart, id).quantity += quant
+    }
+    saveCart()
+}
+
+
+// Create a function that loads the cart from localStorage
+const loadCart = function() {
+    if (localStorage.getItem('cart') === null) {
+            cart = []
+    } else {
+            cart = JSON.parse(localStorage.getItem('cart'))
+    }
+
+    // localStorage.getItem('cart') === null ? cart = [] : cart [] = JSON.parse(localStorage.getItem('cart'))
+}
+
+// Create a function that saves your cart to localStorage
+const saveCart = () => {
+    localStorage.setItem('cart', JSON.stringify(cart))
+}
+
+// Create a function that returns the current shopping cart
+const getCart = () => {
+    return cart
+}
+
+
+// Create a function that calcualtes the total price of the cart
+const calculteTotal = () => {
+    let total = 0
+
+    cart.forEach((item) => {
+            total += item.price * item.quantity
+    })
+
+    return total
+}
+
+
+// Make sure to load the cart array
+loadCart()
