008f90f411695c5760db0fdbce06cf5110e6c4a9
saradelr <saradelr@gmail.com>
2019-04-11 12:49:55 +0200

    capital by url

--- Project/Frontpage/js/products.js
+++ Project/Frontpage/js/products.js
@@ -1,4 +1,8 @@
 let products = []
+const pageUrl = location.href
+
+const capital = pageUrl.substring(pageUrl.indexOf("page/")+5, pageUrl.length - 5 )
+console.log(capital)
 
 
 /**
@@ -21,71 +25,10 @@ const loadProducts = () => {
 
 // Create a function to get the most recent list of products
 function getProducts() {
+    
     return products
 }
 
 // Load the products
 loadProducts()
 
-// Create a function to find a product/item based on it's ID
-// The function should take both the array and ID as inputs
-const findProductById = (data, id) => {
-    const product = data.find((product) => {
-            return product.id === id
-    })
-
-    return product
-}
-
-/**
- * SHOPPING CART
- */
-
-// Create a function that adds a product by it's ID and its quantity to the shopping cart
-const addToCart = (id, quant) => {
-    console.log(`${id} has ben added ${quant} times`);
-    if (!findProductById(cart, id)) {
-            cart.push({...findProductById(products, id), quantity: quant})
-    } else {
-            findProductById(cart, id).quantity += quant
-    }
-    saveCart()
-}
-
-
-// Create a function that loads the cart from localStorage
-const loadCart = function() {
-    if (localStorage.getItem('cart') === null) {
-            cart = []
-    } else {
-            cart = JSON.parse(localStorage.getItem('cart'))
-    }
-
-    // localStorage.getItem('cart') === null ? cart = [] : cart [] = JSON.parse(localStorage.getItem('cart'))
-}
-
-// Create a function that saves your cart to localStorage
-const saveCart = () => {
-    localStorage.setItem('cart', JSON.stringify(cart))
-}
-
-// Create a function that returns the current shopping cart
-const getCart = () => {
-    return cart
-}
-
-
-// Create a function that calcualtes the total price of the cart
-const calculteTotal = () => {
-    let total = 0
-
-    cart.forEach((item) => {
-            total += item.price * item.quantity
-    })
-
-    return total
-}
-
-
-// Make sure to load the cart array
-loadCart()
