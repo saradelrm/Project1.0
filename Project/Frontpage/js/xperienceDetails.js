cdad6c8788e3ad2542d1817970e7cdb29eb31ce2
saradelr <saradelr@gmail.com>
2019-05-02 11:19:34 +0200

    update

--- Project/Frontpage/js/xperienceDetails.js
+++ Project/Frontpage/js/xperienceDetails.js
@@ -6,36 +6,36 @@ const experiences = getAllProducts()
 //console.log(id)
 //console.log(experiences)
 
-const experience = experiences.find(function (experience) { //running the find function for experiences
-    return experience.id === id
-}) 
+        const experience = experiences.find(function (experience) { //running the find function for experiences
+            return experience.id === id
+        }) 
 
-//console.log(experience.name)
+        //console.log(experience.name)
 
-//associating each experience ....?(not sure with what)
-document.getElementById("xp-name").innerText = experience.name
-document.getElementById("xp-title").innerText = experience.title
-document.getElementById("xp-description").innerText = experience.description
-document.getElementById("xp-price").innerText = experience.price
-document.getElementById("xp-rating").src = experience.rating
-document.getElementById("xp-img").src = experience.img_url // experience.img
-document.getElementById("xp-review").innerText = experience.review
+        //associating each experience ....?(not sure with what)
+        document.getElementById("xp-name").innerText = experience.name
+        document.getElementById("xp-title").innerText = experience.title
+        document.getElementById("xp-description").innerText = experience.description
+        document.getElementById("xp-price").innerText = experience.price
+        document.getElementById("xp-rating").src = experience.rating
+        document.getElementById("xp-img").src = experience.img_url // experience.img
+        document.getElementById("xp-review").innerText = experience.review
 
 
-// Add an EventListener to every add-to-cart button in the shop and link it
-// to the addToCart(itemID, quantity)
-const buttons = document.getElementsByClassName('add-cart-btn')
+        // Add an EventListener to every add-to-cart button in the shop and link it
+        // to the addToCart(itemID, quantity)
+        const buttons = document.getElementsByClassName('add-cart-btn')
 
-//console.log(buttons);
+        //console.log(buttons);
 
-//on the click of the button the variable quantity will invoke the function addToCart
-for (let i = 0; i < buttons.length; i++) {
-    buttons[i].addEventListener('click', (event) => {
-        let quantity = buttons[i].previousElementSibling.value
-        addToCart(experience.id, Number(quantity), experience.name)
-        totalAmount = cartTotal()
-    })
-}
+        //on the click of the button the variable quantity will invoke the function addToCart
+        for (let i = 0; i < buttons.length; i++) {
+            buttons[i].addEventListener('click', (event) => {
+                let quantity = buttons[i].previousElementSibling.value
+                addToCart(experience.id, Number(quantity), experience.name)
+                totalAmount = cartTotal()
+            })
+        }
 
 
 
